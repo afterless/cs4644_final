da998692e6824da441b9b4315802be2d0ba83e47
afterless <ayushpanda1029@gmail.com>
2023-05-08 02:57:42 +0000

    new chkpts

--- models/mlp_grok.py
+++ models/mlp_grok.py
@@ -9,9 +9,11 @@ class Embed(nn.Module):
     def __init__(self, d_vocab, d_model):
         super().__init__()
         self.W_E = nn.parameter.Parameter(t.randn(d_model, d_vocab) / d_model**0.5)
+        #self.W_E = nn.parameter.Parameter(t.randn(2, d_model) / d_model**0.5)
 
     def forward(self, x: t.LongTensor):
         return t.einsum("dbp->bpd", self.W_E[:, x])
+        #return t.einsum("bp,pd->bd", x, self.W_E)
 
 
 class UnEmbed(nn.Module):
@@ -19,33 +21,34 @@ class UnEmbed(nn.Module):
         super().__init__()
         self.W_U = nn.parameter.Parameter(t.randn(d_model, d_vocab) / d_vocab**0.5)
 
-    def forward(self, x):
+    def forward(self, x: t.LongTensor):
         return t.matmul(x, self.W_U)
 
 
-# TODO: Needs to be fixed to match spec
 class MLP(nn.Module):
     def __init__(self, d_vocab, d_model):
         super().__init__()
         self.embed = Embed(d_vocab, d_model)
         self.layer0 = nn.Linear(d_model + d_model, d_model * 2, bias=False)
         self.layer1 = nn.Linear(d_model * 2, d_model, bias=False)
-        self.unembed = UnEmbed(d_vocab, d_model)
-        # self.hook0 = HookPoint()
+        self.unembed = nn.Linear(d_model, d_vocab, bias=False)
+
+        self.hook0 = HookPoint()
         self.hook1 = HookPoint()
         self.hook2 = HookPoint()
         self.hook3 = HookPoint()
 
-        # self.hook0.give_name("embed")
+        self.hook0.give_name("embed")
         self.hook1.give_name("layer0")
         self.hook2.give_name("layer1")
         self.hook3.give_name("unembed")
 
     def forward(self, x):
-        x = self.embed(x)
+        x = self.hook0(self.embed(x))
         x = rearrange(x, "b s d -> b (s d)")
         # x = x.sum(dim=1)
         x = F.relu(self.hook1(self.layer0(x)))
         x = F.relu(self.hook2(self.layer1(x)))
         x = self.hook3(self.unembed(x))
+        print(x.shape)
         return F.log_softmax(x, dim=-1)
